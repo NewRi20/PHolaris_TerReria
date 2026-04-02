6082d1b4db90a1f4e3126a1b70827b3181487275
Wen <frondairwen@gmail.com>
2026-04-02 16:13:43 +0800

    tailwind css config

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,10 @@
-
 function App() {
-  
   return (
-    <></>
+    <div className="flex h-screen items-center justify-center bg-zinc-900">
+      <h1 className="text-4xl font-bold text-blue-400">
+        Tailwind is working! 🚀
+      </h1>
+    </div>
   )
 }
 
