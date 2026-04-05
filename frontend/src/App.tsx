173dc40a28c49b5a530113d3c64d8da677afa9fc
Wen <frondairwen@gmail.com>
2026-04-05 17:43:59 +0800

    tc/admin proper router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,44 +1,7 @@
-import { useState } from 'react';
-
-import AdminLayout from './layouts/AdminLayout';
-import TeacherLayout from './layouts/TeacherLayout';
-
-import AdminDashboard from "./components/AdminDashboard";
-import UnderservedAreas from "./components/UnderservedAreas";
-import EventsManagement from "./components/EventsManagement";
-import TeacherDirectory from "./components/TeacherDirectory";
-import TeacherDashboard from "./components/TeacherDashboard";
-import TeacherProfile from "./components/TeacherProfile";
+import { AppRouter } from './router';
 
 function App() {
-  // Set your default starting view here
-  const [currentView, setCurrentView] = useState('admin-dashboard');
-
-  const renderAdminView = () => {
-    switch (currentView) {
-      case 'admin-dashboard': return <AdminDashboard />;
-      case 'underserved-areas': return <UnderservedAreas />;
-      case 'events-management': return <EventsManagement />;
-      case 'teacher-directory': return <TeacherDirectory />;
-      default: return <AdminDashboard />;
-    }
-  };
-
-  // Switch to Teacher Portal view
-  if (currentView === 'teacher-portal') {
-    return (
-      <TeacherLayout currentView={currentView} setCurrentView={setCurrentView}>
-        <TeacherDashboard />
-      </TeacherLayout>
-    );
-  }
-
-  // Default Admin Portal view
-  return (
-    <AdminLayout currentView={currentView} setCurrentView={setCurrentView}>
-      {renderAdminView()}
-    </AdminLayout>
-  );
+  return <AppRouter />;
 }
 
 export default App;
